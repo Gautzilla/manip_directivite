9a4b5b83aeb16375627461a6930803874056bb0c
Gautzilla <gauthier.berthomieu@gmail.com>
2024-06-07 15:12:45 +0200

    add user to db method

--- model/db.py
+++ model/db.py
@@ -3,10 +3,13 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 import datetime
 
 Base = declarative_base()
+Session = None
 
 def initialize_db():    
+    global Session    
     engine = create_engine('sqlite:///model/manip_directivite.db', echo = True)
     Base.metadata.create_all(bind = engine)
+    Session = sessionmaker(bind = engine)
 
 class User(Base):
     __tablename__ = 'users'
@@ -16,7 +19,20 @@ class User(Base):
     last_name = Column('last_name', VARCHAR(200))
     birth_date = Column('birth_date', DATE)
 
-    def __init__(self, first_name, last_name, birth_date):
+    def __init__(self, first_name: str, last_name: str, birth_date: datetime):
         self.first_name = first_name
         self.last_name = last_name
         self.birth_date = birth_date
+
+def create_new_user(first_name: str, last_name: str, birth_date: datetime):
+    user = User(first_name, last_name, birth_date)
+    add_to_db(user)
+
+def add_to_db(object):
+    global Session
+    session = Session()
+    try:
+        session.add(object)
+        session.commit()
+    finally:
+        session.close()
