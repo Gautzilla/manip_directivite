0ee4ff551082946a9632e7df39d49211724ae9d7
Gautzilla <gauthier.berthomieu@gmail.com>
2024-06-14 15:40:21 +0200

    link view and model through controller

---
+++ model/app_model.py
@@ -0,0 +1,7 @@
+from model import db_constants_filler
+
+class AppModel():
+    
+    @staticmethod
+    def initialize_db():
+        db_constants_filler.initialize_db()
---
+++ view/app_view.py
@@ -0,0 +1,4 @@
+import customtkinter as ctk
+
+class AppView(ctk.CTk):
+    pass
