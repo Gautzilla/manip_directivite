1e9345f3e09ba94d11c723facc38065e01b936ca
Gautzilla <gauthier.berthomieu@gmail.com>
2024-07-17 13:54:27 +0200

    add visual feedback to direct question answer

--- src/view/ratings_view.py
+++ src/view/ratings_view.py
@@ -32,12 +32,20 @@ class DirectQuestion(ctk.CTkFrame):
         self.answer_callback = answer_callback
         self.answer = None
         self.correct_answer = None
-        
-        self.choice_1 = ctk.CTkButton(master = self, text = self.choices[0], command = self.set_choice(choice = 0))
-        self.choice_1.grid_configure(row = 0, column = 0, padx = (10,0), pady = 10, sticky = 'nw')
 
-        self.choice_2 = ctk.CTkButton(master = self, text = self.choices[1], command = self.set_choice(choice = 1))
-        self.choice_2.grid_configure(row = 0, column = 1, padx = 10, pady = 10, sticky = 'ne')
+        self.columnconfigure((0,3), weight = 1)
+        
+        self.choice_1 = ctk.CTkButton(master = self, text = self.choices[0])
+        self.choice_1.grid_configure(row = 0, column = 1, padx = (10,0), pady = 10, sticky = 'nw')
+
+        self.choice_2 = ctk.CTkButton(master = self, text = self.choices[1])
+        self.choice_2.grid_configure(row = 0, column = 2, padx = 10, pady = 10, sticky = 'ne')
+
+        self.choice_buttons = (self.choice_1, self.choice_2)
+
+        for index, button in enumerate(self.choice_buttons):
+            button.configure(command = lambda index = index: self.set_choice(index), hover = False)
+            button.configure(fg_color = 'grey25')
 
     def set_correct_answer(self, answer: int):
         self.correct_answer = answer
@@ -46,6 +54,11 @@ class DirectQuestion(ctk.CTkFrame):
     def set_choice(self, choice: int):
         self.answer = choice == self.correct_answer
         self.answer_callback()
+        
+        not_chosen = (choice-1)**2
+
+        self.choice_buttons[choice].configure(fg_color = '#00966b')
+        self.choice_buttons[not_chosen].configure(fg_color = 'grey25')
         
 
 class RatingsView(ctk.CTkFrame):
