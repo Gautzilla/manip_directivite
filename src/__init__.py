b0e516e7ff54023e365fc18e3c360b199de5bbe6
Gautzilla <gauthier.berthomieu@gmail.com>
2024-07-22 11:28:41 +0200

    add dev global constant
    The display of development features (recording filtering, soundfile display...) should rely on this constant

--- src/__init__.py
+++ src/__init__.py
@@ -5,3 +5,5 @@ DATABASE_PATH = path.join(DATA_PATH, 'manip_directivite.db')
 AUDIO_FOLDER = path.join(DATA_PATH, 'audio')
 ASSETS_FOLDER = path.join(DATA_PATH, 'assets')
 SENTENCES_CSV_FILE = path.join(DATA_PATH, 'sentences.csv')
+
+DEV = False
